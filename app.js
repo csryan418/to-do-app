4db76fadeb0943ab7a4e1129ae0e0836d8b1050e
Caroline <csryan418@gmail.com>
2018-09-27 12:25:33 -0400

    To Do App Part 2 assignment: completed

--- app.js
+++ app.js
@@ -1,48 +1,63 @@
+
 function onReady() {
-  const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
+  let toDos = [];
+  let id = 0;
 
   function createNewToDo() {
     const newToDoText = document.getElementById('newToDoText');
-    if (!newToDoText.value) { return; }
+    if(!newToDoText.value){ return; }
 
     toDos.push({
-      title: newToDoText.value,
-      complete: false
+        title: newToDoText.value,
+        complete: false,
+        id: ++id
     });
 
     newToDoText.value = '';
+    renderTheUI();
 
-    rendertheUI();
-  }
+}
 
-    function renderTheUI() {
-      const toDoList = document.getElementById('toDoList');
-
-      toDoList.textContent = '';
-
-      toDos.forEach(function(toDo){
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+    toDoList.textContent = '';
+      toDos.forEach(function(toDo) {
         const newLi = document.createElement('li');
         const checkbox = document.createElement('input');
         checkbox.type = "checkbox";
+
+        const deleteButton = document.createElement("button");
+        deleteButton.type = "button";
+        deleteButton.textContent = 'Delete';
 
         newLi.textContent = toDo.title;
 
         toDoList.appendChild(newLi);
         newLi.appendChild(checkbox);
-      });
+        newLi.appendChild(deleteButton);
 
-    }
+        deleteButton.addEventListener('click', function() {
+          deleteToDo(toDo.id);
+          renderTheUI();
+        });
+
+
+    });
   }
+
+    function deleteToDo (id){
+      toDos = toDos.filter(item => item.id !== id);
+    }
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
     createNewToDo();
   });
-
-  renderTheUI();
+    renderTheUI();
 }
+
 
 window.onload = function() {
   onReady();
-};
+  };
